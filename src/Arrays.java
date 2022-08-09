ad855ac951b80016986dce6acaa4afbab374ee77
Madhumitha-1 <Madhumitha.Sindam@ivycomptech.com>
2022-08-09 14:31:42 +0530

    commit

---
+++ src/Arrays.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Arrays {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=10;
+        int student[][]=new int[10][4];
+        for(int i=0;i<10;i++){
+            student[i][0]=1000+i;
+            for(int j=1;j<=3;j++){
+                student[i][j]=sc.nextInt();1
+            }
+        }
+    }
+}
---
+++ src/Strings.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string1=sc.next();
+        String string2=sc.next();
+        //lentgh
+        System.out.println("String1 length is "+string1.length());
+        System.out.println("String2 length is "+string2.length());
+        //Merge
+        String string3 = string1+" "+string2;
+        System.out.println(string3);
+        //split
+        String splitString[] = string3.split(regex:" ")
+
+    }
+}
